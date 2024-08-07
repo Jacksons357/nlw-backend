b01c63dcdadee54ea38fbf3e1c31e6437c5e4f15
Jackson <jacksyntax20@gmail.com>
2024-08-06 22:15:29 -0300

    create activities database and route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import fastify from "fastify"
-import { createTrip } from "./routes/create-trip"
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
-import { confirmTrip } from "./routes/confirm-trip"
 import cors from '@fastify/cors'
+import fastify from "fastify"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import { confirmParticipant } from "./routes/confirm-participant"
+import { confirmTrip } from "./routes/confirm-trip"
+import { createActivity } from "./routes/create-activity"
+import { createTrip } from "./routes/create-trip"
+import { getActivities } from "./routes/get-activities"
 
 const app = fastify()
 
@@ -15,6 +18,9 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
