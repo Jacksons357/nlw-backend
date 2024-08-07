66471fca2220ec582408bc6f39a25fd5842c504a
Jackson <jacksyntax20@gmail.com>
2024-08-07 18:43:08 -0300

    create routes for links and activities

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import { confirmTrip } from "./routes/confirm-trip"
 import { createActivity } from "./routes/create-activity"
 import { createTrip } from "./routes/create-trip"
 import { getActivities } from "./routes/get-activities"
+import { createLink } from './routes/create-link'
+import { getLinks } from './routes/get-links'
 
 const app = fastify()
 
@@ -21,6 +23,8 @@ app.register(confirmTrip)
 app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
